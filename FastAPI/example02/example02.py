9d6729be3882f5bb21ab26dd829abd4d0479d118
jelg <perpetualmoti0n@outlook.com>
2025-06-19 13:59:38 -0600

    example02 modified

--- FastAPI/example02/example02.py
+++ FastAPI/example02/example02.py
@@ -6,4 +6,34 @@ async def root(id):
     return {"car_id":id}
 
 
+@app.get("/books/{book_id}")
+async def read_books(book_id:int):
+    return {
+        "book_id": book_id,
+        "title": "Alice´s Adventures in Worderland",
+        "author": "Lewis Carroll"
+    }
+
+@app.get("/authors/{author_id}")
+async def read_author(author_id:int):
+    return {
+        "author_id": author_id,
+        "author": "Lewis Carroll"
+    }
+
+
+@app.get("/books2")             #books2?genere=action&year=2025
+async def read_books(year: int = None):
+    if year:
+        return {
+            "year": year,
+            "books": ["Book 1", "Book 2"]
+        }
+    return {"books":["All Books"]} #if there is no year in the link, it returns "All book"
+
+
+
+
+
+
     
