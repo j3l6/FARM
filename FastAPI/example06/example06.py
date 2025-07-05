ca507b169b024dbc38e737c07744b4245de6414d
jelg <perpetualmoti0n@outlook.com>
2025-07-05 16:03:03 -0600

    example06 added

---
+++ FastAPI/example06/example06.py
@@ -0,0 +1,9 @@
+from typing import Dict
+from fastapi import FastAPI, Body
+
+app = FastAPI()
+@app.post("/cars")
+async def new_car(data: Dict = Body(...)):
+    print(data)
+    return {"message": data}
+
