6e202f351b9415c9f20d0ab6ffb8a22250cc49e7
jelg <perpetualmoti0n@outlook.com>
2025-07-05 09:19:23 -0600

    example05 added

---
+++ FastAPI/example05/example05.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/cars/price")
+
+async def cars_by_price(min_price: int = 0, max_price:int = 100000):
+
+    if min_price < 0 | max_price > 100000: #validation
+        return {"Message": "Error"}
+    else:
+        return {"Message": f"Listining cars with prices between {min_price} and {max_price}"}
+
