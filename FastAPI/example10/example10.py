38d561e359a9ccfcea70a0e47f3e98ecd311bed4
jelg <perpetualmoti0n@outlook.com>
2025-07-07 13:54:09 -0600

    Add example10

---
+++ FastAPI/example10/example10.py
@@ -0,0 +1,9 @@
+from typing import Annotated
+from fastapi import FastAPI, Header
+
+app = FastAPI()
+
+@app.get("/headers")
+async def read_header(user_agent: Annotated[str | None, Header()] = None):
+    return {"User-Agent": user_agent }
+
