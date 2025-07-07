8e5986543f9a33a08394660643ac2945d3e7dca6
jelg <perpetualmoti0n@outlook.com>
2025-07-07 13:55:16 -0600

    Add example11

---
+++ FastAPI/example11/example11.py
@@ -0,0 +1,6 @@
+from fastapi import FastAPI, Form, File, UploadFile
+
+app = FastAPI()
+@app.post("/upload")
+async def upload(file: UploadFile = File(...), brand: str = Form(...), model: str = Form(...)):
+    return {"brand": brand, "model": model, "file_name": file.filename}
