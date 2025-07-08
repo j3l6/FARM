c5d5faee6ee066e2c3f6053205934dd8e65ce188
jelg <perpetualmoti0n@outlook.com>
2025-07-08 13:50:53 -0600

    Add comments

--- FastAPI/example12/example12.py
+++ FastAPI/example12/example12.py
@@ -8,6 +8,6 @@ async def upload(
     picture: UploadFile = File(...),
     brand: str = Form(...),
     model: str = Form(...)):
-    with open ("saved_file.png", "wb") as buffer:
-        shutil.copyfileobj(picture.file, buffer)
+    with open ("saved_file.png", "wb") as buffer: #file-like object is asiggned to the variable buffer
+        shutil.copyfileobj(picture.file, buffer)  #copy the original file into the variable buffer
         return {"brand": brand, "model": model, "file_name": picture.filename}
